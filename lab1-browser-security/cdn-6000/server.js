ddfb0deb9f43dd6fd830018138997b840d6172ac
qazxcvbnjhytr <nataliasumska95@gmail.com>
2026-03-12 04:59:11 +0200

    Lab3: додано breach-режим для CDN сервера на порту 6000

--- lab1-browser-security/cdn-6000/server.js
+++ lab1-browser-security/cdn-6000/server.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+const publicDir = path.join(__dirname, "public");
+const configPath = path.join(__dirname, "config.json");
+const reactMockPath = path.join(publicDir, "react-mock.js");
+const breachedScript = 'alert("CRITICAL: CDN Compromised! Stealing data...");\n';
 
-app.listen(7000, () => {
-  console.log("CDN running on http://localhost:7000");
+function readConfig() {
+  return JSON.parse(fs.readFileSync(configPath, "utf-8"));
+}
+
+app.get("/react-mock.js", (req, res) => {
+  const config = readConfig();
+
+  res.type("application/javascript");
+
+  if (config.mode === "breach") {
+    return res.send(breachedScript);
+  }
+
+  return res.sendFile(reactMockPath);
+});
+
+app.use(express.static(publicDir));
+
+[6000, 7000].forEach((port) => {
+  app.listen(port, () => {
+    console.log(`CDN running on http://localhost:${port}`);
+  });
 });
